8bf1edc95f117c3091e344ea2072975b3abb6573
mori <chohyuk0725@gmail.com>
2022-05-14 22:36:09 +0900

    eighth commit

---
+++ .history/typescript-practice/class-note/2_functions_20220514223500.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+sum(10, 20);
+
+function add(): number {}
---
+++ .history/typescript-practice/class-note/2_functions_20220514223513.ts
@@ -0,0 +1,8 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+sum(10, 20);
+
+function add(): number {
+  return 10;
+}
