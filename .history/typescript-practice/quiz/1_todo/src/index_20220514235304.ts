c8d38df3544b9a8109a85905de1ac3155a6d0fa7
mori <chohyuk0725@gmail.com>
2022-05-14 23:57:23 +0900

    eleventh commit

--- .history/typescript-practice/quiz/1_todo/src/index_20220514235304.ts
+++ .history/typescript-practice/quiz/1_todo/src/index_20220514235304.ts
@@ -39,19 +39,12 @@ function showCompleted():object[] {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems():void {
+function addTwoTodoItems() {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
   const item1 ={
-    id:4,
-    title:"아이템 4",
-    done:false
+    id:4
   }
-  addTodo(item1)
-  addTodo({
-    id:5,
-    title:"아이템 5",
-    done:false
-  })
+  addTodo()
 }
 
 // NOTE: 유틸 함수
